a63c2b1e8ae61bd8d37aa0783a4ff8422b62e22d
ManelDerbel <manel11derbel@gmail.com>
2025-11-02 17:32:14 +0400

    Add Part3-CrossBrowserTesting+ReadingPropertiesFile of Project

---
+++ Udemy-Training/Session-3/Part3-CrossBrowser+PropertiesFile/src/test/java/pageObjects/HomePage.java
@@ -0,0 +1,26 @@
+package pageObjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class HomePage extends BasePage {
+	
+	public HomePage(WebDriver driver) {
+		super(driver);
+	}
+	
+	@FindBy(xpath="//span[text()='My Account']")
+	WebElement linkAccount;
+	
+	@FindBy(xpath="//a[text()='Register']")
+	WebElement linkRegister;
+	
+	public void clickMyAccount() {
+		linkAccount.click();
+	}
+	
+	public void clickRegister() {
+		linkRegister.click();
+	}
+}
